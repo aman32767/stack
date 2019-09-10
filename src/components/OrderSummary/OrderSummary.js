85570c76838286d911f6238a32aefc2e6e03128f
aman <writetoaman99@gmail.com>
2019-09-10 12:50:47 +0530

    finalised basic verion of app

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux/Aux'
 import Button from '../UI/Button/Button'
 import classes from '../UI/Button/Button.css'
 const orderSummary = (props)=>{
