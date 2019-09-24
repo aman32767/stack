606866e39fe0b0ef25cbbde8015c9615f37ee438
aman <writetoaman99@gmail.com>
2019-09-24 21:44:17 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
+import { Route , Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/" component={BurgerBuilder} />
+            <Route path="/myorder" component={Checkout} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import { BrowserRouter } from 'react-router-dom';
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+ReactDOM.render(app, document.getElementById('root'));
